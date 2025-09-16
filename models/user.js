cd24454f96220c865bf05fc95db2ec59d8e3e0a9
Abdulla Dashti <abdullamustafadashti.0@gmail.com>
2025-09-16 17:28:11 +0300

    Schema Added

--- models/user.js
+++ models/user.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose');
 
+const foodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +20,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  pantry: [foodSchema]
 });
 
 const User = mongoose.model('User', userSchema);
